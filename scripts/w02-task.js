1dfdc1f3786203e18216c662c37e41ea49b6b788
jocygrajeda <gra23004@byui.edu>
2023-09-23 21:56:58 -0500

    fixed img errpr

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -1,3 +1,5 @@
+console.log("Script is running");
+
 /* W02-Task - Profile Home Page */
 
 /* Step 1 - Setup type tasks - no code required */
@@ -14,14 +16,15 @@
  const foodElement = document.getElementById('food');
  //Note the # in the selector string to indicate the id attribute value.
  const yearElement = document.querySelector('#year');
- const imageElement = document.getElementById('profileImage');
- 
+ const imageElement = document.querySelector('picture img');
+
  /* Step 4 - Adding Content */
  nameElement.innerHTML = `<strong>${fullName}</strong>`;
  yearElement.textContent = currentYear;
- imageElement.setAttribute('src', profilePicture);
- imageElement.setAttribute('alt', `Profile image of ${fullName}`);
- 
+ //imageElement.setAttribute('src', profilePicture);
+ //imageElement.setAttribute('alt', `Profile image of ${fullName}`);
+ imageElement.src = 'images/IMG_2246.png';
+ imageElement.alt = 'Profile image of Jocelyne Sainz';
  /* Step 5 - Array */
  const favoriteFoods = ['tacos', 'quesibirria', 'tteokbokki', 'onigiri', 'strawberry', 'mango', 'menudo'];
  favoriteFoods.forEach(food => {
