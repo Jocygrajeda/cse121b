9b64b0ed99015c300960f91b7baf83f824918412
jocygrajeda <gra23004@byui.edu>
2023-09-29 17:32:15 -0500

    add numbers

---
+++ scripts/w03-task.js
@@ -0,0 +1,35 @@
+/* LESSON 3 - Programming Tasks */
+
+/* FUNCTIONS */
+/* Function Definition - Add Numbers */
+function add (number1, number2) {
+    //this is the function body
+    //return the sum of the parameters num1 & num2
+    return number1 + number2;
+}
+
+let addNumbers
+/* Function Expression - Subtract Numbers */
+
+
+/* Arrow Function - Multiply Numbers */
+
+
+/* Open Function Use - Divide Numbers */
+
+
+/* Decision Structure */
+
+
+/* ARRAY METHODS - Functional Programming */
+/* Output Source Array */
+
+/* Output Odds Only Array */
+
+/* Output Evens Only Array */
+
+/* Output Sum of Org. Array */
+
+/* Output Multiplied by 2 Array */
+
+/* Output Sum of Multiplied by 2 Array */
