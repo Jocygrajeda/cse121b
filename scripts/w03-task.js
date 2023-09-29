9915bab90a8ab467f9714194a29b507c329a1079
jocygrajeda <gra23004@byui.edu>
2023-09-29 18:45:06 -0500

    new

--- scripts/w03-task.js
+++ scripts/w03-task.js
@@ -8,14 +8,25 @@ function add(number1, number2) {
     return number1 + number2;
 }
 
+
+// Using a function declaration, define another function named addNumbers
+//that gets the values of two HTML form controls with IDs of add1 and add2. 
 function addNumbers() {
     let addNumber1 = Number(document.querySelector('#add1').value);
     let addNumber2 = Number(document.querySelector('#add2').value);
 
+
+    //Next, in the addNumbers function, call the add function using
+    //those two arguments and assign the return value to an HTML form element with an ID of sum
     document.querySelector('#sum').value = add(addNumber1, addNumber2);
 }
 
+//Add a "click" event listener to the HTML button with an ID of addNumbers that calls the 
+//addNumbers function. This line of code is NOT located inside a function. 
 document.querySelector('#addNumbers').addEventListener('click', addNumbers);
+
+
+//step 3
 /* Function Expression - Subtract Numbers */
 
 
