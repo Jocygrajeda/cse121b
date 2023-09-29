c85b67743b47e38c5d0667193d455433107ab31f
jocygrajeda <gra23004@byui.edu>
2023-09-29 18:45:06 -0500

    part 3

--- scripts/w03-task.js
+++ scripts/w03-task.js
@@ -8,18 +8,42 @@ function add(number1, number2) {
     return number1 + number2;
 }
 
+
+// Using a function declaration, define another function named addNumbers
+//that gets the values of two HTML form controls with IDs of add1 and add2. 
 function addNumbers() {
     let addNumber1 = Number(document.querySelector('#add1').value);
     let addNumber2 = Number(document.querySelector('#add2').value);
 
+
+    //Next, in the addNumbers function, call the add function using
+    //those two arguments and assign the return value to an HTML form element with an ID of sum
     document.querySelector('#sum').value = add(addNumber1, addNumber2);
 }
 
+//Add a "click" event listener to the HTML button with an ID of addNumbers that calls the 
+//addNumbers function. This line of code is NOT located inside a function. 
 document.querySelector('#addNumbers').addEventListener('click', addNumbers);
+
+
+//step 3
 /* Function Expression - Subtract Numbers */
 
+// Function Expression for subtraction
+const subtract = function(number1, number2) {
+    return number1 - number2;
+  };
+  
+// Function Expression for getting values from HTML form controls and performing subtraction
+const subtractNumbers = function() {
+// Get values from HTML form controls and convert to numbers
+    let subtractNumber1 = Number(document.querySelector('#subtract1').value);
+    let subtractNumber2 = Number(document.querySelector('#subtract2').value);
 
-
+    document.querySelector('#difference').value = subtract(subtractNumber1, subtractNumber2);
+};
+  
+  document.querySelector('#subtractNumbers').addEventListener('click', subtractNumbers);
 /* Arrow Function - Multiply Numbers */
 
 
