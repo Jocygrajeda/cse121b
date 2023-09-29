4f8d282119412b57518981f4438d9569970e6cf5
jocygrajeda <gra23004@byui.edu>
2023-09-29 18:22:25 -0500

    function

--- scripts/w03-task.js
+++ scripts/w03-task.js
@@ -2,14 +2,22 @@
 
 /* FUNCTIONS */
 /* Function Definition - Add Numbers */
-function add (number1, number2) {
+function add(number1, number2) {
     //this is the function body
     //return the sum of the parameters num1 & num2
     return number1 + number2;
 }
 
-let addNumbers
+function addNumbers() {
+    let addNumber1 = Number(document.querySelector('#add1').value);
+    let addNumber2 = Number(document.querySelector('#add2').value);
+
+    document.querySelector('#sum').value = add(addNumber1, addNumber2);
+}
+
+document.querySelector('#addNumbers').addEventListener('click', addNumbers);
 /* Function Expression - Subtract Numbers */
+
 
 
 /* Arrow Function - Multiply Numbers */
