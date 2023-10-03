510b4cf3feca682f48f7ed25c0d8c8a149d41b64
jocygrajeda <gra23004@byui.edu>
2023-10-03 17:13:20 -0500

    added w4 html and js

---
+++ scripts/w04-task.js
@@ -0,0 +1,28 @@
+/* LESSON 3 - Programming Tasks */
+
+/* Profile Object  */
+
+
+
+
+/* Populate Profile Object with placesLive objects */
+
+
+
+
+/* DOM Manipulation - Output */
+
+/* Name */
+
+/* Photo with attributes */
+
+
+/* Favorite Foods List*/
+
+
+/* Hobbies List */
+
+
+/* Places Lived DataList */
+
+
