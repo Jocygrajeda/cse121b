ca947d7a024eab526bfe3ff9123134f9eaa08ad7
Jocygrajeda <122635036+Jocygrajeda@users.noreply.github.com>
2023-10-24 19:08:36 -0500

    diff color

--- project/app.js
+++ project/app.js
@@ -12,9 +12,9 @@ document.addEventListener('DOMContentLoaded', function () {
     datasets: [{
       label: 'Minutes of Exercise',
       data: Array(30).fill(0),
-      backgroundColor: 'rgba(75, 192, 192, 0.2)',
-      borderColor: 'rgba(75, 192, 192, 1)',
-      borderWidth: 1,
+      backgroundColor: 'rgba(75, 192, 112, 0.2)',
+      borderColor: 'rgba(28, 50, 0, 7)',
+      borderWidth: 5,
     }],
   };
 
